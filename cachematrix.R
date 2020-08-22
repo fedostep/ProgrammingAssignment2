47d9d5996d138bddb83808503a03a31d500859bd
fedostep <61801315+fedostep@users.noreply.github.com>
2020-08-22 08:51:13 +0300

    update2

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,14 @@
+## This functions pretend to reduce the computational cost of computing
+## repeatedly the inverse of a matrix. Instead, these functions allow 
+## the process take place in two steps. 
+
 makeCacheMatrix <- function(x = matrix()) {
+  ## Firstly, the makeCacheMatrix function pretends to create a matrix, also
+  ## calculated and stores the inverse of that matrix lization of the variable that will store the inverse matrix 
   j <- NULL
+  #The set function assign the the values from the parent environment
+  #If there is already a matrix in matr it will clear it, in order 
+  #recalculate the inverse.This is done when the initial matrix is change.
   set <- function(y){
     x <<- y
     j <<- NULL
@@ -11,8 +20,9 @@ makeCacheMatrix <- function(x = matrix()) {
        setInverse = setInverse, 
        getInverse = getInverse)
 }
-
-##Please include your own comment to explain your code (Required in Rubric)
+## Firstly, the cacheSolve function pretends to create a matrix, also
+## calculated and stores the inverse of the matrix of the MakeCacheMatrix
+## function. The function has an object as main argument
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
